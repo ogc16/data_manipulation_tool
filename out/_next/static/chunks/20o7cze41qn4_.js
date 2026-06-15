492b5c03ee46b0c288b6d8041f26e75752319508
Caleb Ngeno <82827770+ogc16@users.noreply.github.com>
2026-06-15 11:47:40 +0300

    Commit built static export for Pages deployment

---
+++ out/_next/static/chunks/20o7cze41qn4_.js
@@ -0,0 +1,1 @@
+(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,55115,t=>{t.v(e=>Promise.all(["static/chunks/38l04e3yh7wzq.js"].map(e=>t.l(e))).then(()=>e(72833)))}]);
